512ae2933ca9f5a2b1bc9eff9063303cb8bc2ced
macnum <w.damilola@yahoo.com>
2022-04-13 16:53:28 +0000

    Fix bracket bug

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,13 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * _isalpha  - checks for any alphabetic character
- * @c: character to check
- *
- * Return: 1 if c is a letter, 0 otherwise
+ * _isalpha - a function that checks for alphabetic character
+ * @c: single letter input
+ * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
-
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 		return (1);
-	}
-
-	return (0);
+	else
+		return (0);
+}
