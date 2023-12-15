d89f90b8fc53bb33e554f2018639f01eae422997
macnum <w.damilola@yahoo.com>
2023-12-15 13:18:31 +0000

    Add task 5: Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a string of characters
+ * Return: on success (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
