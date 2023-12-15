31f081bd0e821bfb610ebbe3f1a98fa7db9a0957
macnum <w.damilola@yahoo.com>
2023-12-15 12:22:55 +0000

    Add task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - This prints out a set of strings
+ * Description: it outputs a set of characters strings and ends with a line
+ * Return: if successful (0)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	puts("\n");
+
+	return (0);
+}
