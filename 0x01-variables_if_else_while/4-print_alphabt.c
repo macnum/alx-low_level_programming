e77108d490debce3c96013d51cf78319a60db258
macnum <w.damilola@yahoo.com>
2023-12-19 16:17:10 +0000

    Add task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints a string
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (!(c == 'q' || c == 'e'))
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
