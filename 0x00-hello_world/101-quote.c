a023aa60f62ad60332e932264f345846c1a1c596
macnum <w.damilola@yahoo.com>
2023-12-19 11:02:20 +0000

    Update characters len

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-
+/**
+ * main - prints a set of charactyer strings to the stdout
+ * Return: 0 (success)
+ */
 int main(void)
 {
-	char* value = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen(value));
+	char *value = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	strlen(value));
 	return (1);
 }
