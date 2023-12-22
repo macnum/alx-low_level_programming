8c38eccb9cfb78a4cd1e123caac7d1cc02f81513
macnum <w.damilola@yahoo.com>
2023-12-22 11:47:59 +0000

    Add task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - test for positive or negative function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
