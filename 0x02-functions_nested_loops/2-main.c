7cf9706172cf4d9ef3ad9591f64a7fb4853108e9
macnum <w.damilola@yahoo.com>
2023-12-27 22:06:19 +0000

    Add task 2

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,11 +1,12 @@
 #include "main.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
 
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	print_alphabet_x10();
-
-	return (0);
+    print_alphabet_x10();
+    return (0);
 }
