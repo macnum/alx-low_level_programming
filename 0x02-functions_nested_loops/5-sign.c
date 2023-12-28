b6a453cf84d52a532c90499b8c7e2429678b7c1e
macnum <w.damilola@yahoo.com>
2023-12-28 01:48:21 +0000

    Add task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_sign - this prints the sign of a number
+  * @n: this a variable
+  * Return: (0) success
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
