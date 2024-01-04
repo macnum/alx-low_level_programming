6515de93aca86af741fa92099131c49f35e1dc6f
macnum <w.damilola@yahoo.com>
2024-01-04 11:51:34 +0000

    Add task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - takes a pointer to an int and updates the value
+  * @n: pointer integer
+  * Return: (0)on sucess
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
