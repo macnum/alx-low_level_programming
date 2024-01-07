dd75f8483404fd9d0c84984b5283e0ffbaa3f92f
macnum <w.damilola@yahoo.com>
2024-01-07 14:55:47 +0000

    Update code

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,15 +6,10 @@
   * Return: (0) success
   */
 
-int _abs(int)
+int _abs(int n)
 {
 
-	if (n >= 0)
-	{
-		return (n);
-	}
-	else if (n < 0)
-	{
+	if (n < 0)
 		return (n * -1);
-	}
+	return (n);
 }
