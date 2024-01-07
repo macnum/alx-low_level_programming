9c68a780c501a77a0226f454138e1776fbe09132
macnum <w.damilola@yahoo.com>
2024-01-07 14:51:12 +0000

    Add task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _abs - this prints the sign of a number
+  * @int: variable
+  * Return: (0) success
+  */
+
+int _abs(int)
+{
+
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (n * -1);
+	}
+}
